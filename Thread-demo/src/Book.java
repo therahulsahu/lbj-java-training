ad6dc3496dd9eaf9736d804bb0b4bbe295a855d2
Rahul Sahu <therahulsahu7@gmail.com>
2022-03-31 14:18:29 +0530

    using checked exception

---
+++ Thread-demo/src/Book.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+
+enum STATUS {
+	NEW, DAMAGED, RETURN
+}
+
+public class Book {
+	private String title;
+	private float price;
+	private STATUS status; // new, damaged, return
+	
+	public Book() {
+		this.status = STATUS.NEW;
+	}
+	
+	public Book(float price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Book price : " + this.price ;
+	}
+	
+	public STATUS getStatus() {
+		return status;
+	}
+
+	public void setStatus(STATUS status) {
+		this.status = status;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public float getPrice() {
+		return price;
+	}
+	
+	public void setPrice(float price) throws IOException {
+		if(price <= 0) {
+			// IOException is a compile time exception
+			// It needs to be handled with try catch or deferred
+			throw new IOException("Price > 0");
+			
+		}
+		this.price = price;
+	}
+}
