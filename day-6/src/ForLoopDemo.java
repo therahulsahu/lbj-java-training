faaf35494614094c85d8dbd7fc515a079c9fa776
Rahul Sahu <therahulsahu7@gmail.com>
2022-04-04 12:17:24 +0530

    added function for writing pattern to file

--- day-6/src/ForLoopDemo.java
+++ day-6/src/ForLoopDemo.java
@@ -1,9 +1,22 @@
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class ForLoopDemo {
-	public static void main(String[] args) {
-		printTriangleUsingWhileLoop(5);
+	public static void main(String[] args) throws IOException {
+		
+		
+		System.out.print("Input the character to be in the pattern : ");
+		Scanner sc = new Scanner(System.in);
+		char character = sc.next().charAt(0);
+		
+		writeFile("output.txt", stringTriangle(5, character));
+		
+		sc.close();
+		System.out.println("Done, Check the pattern in output.txt");
 	}
+	
 	
 	/**
 	 * Prints a triangle pattern of "size" rows and "size" columns
@@ -22,6 +35,34 @@ public class ForLoopDemo {
 		
 	}
 	
+	private static String stringTriangle(int size) {
+		String pattern = "";
+		if(size <= 0) {
+			throw new IllegalArgumentException("Size should be greater than 0");
+		}
+		for(int row = size; row > 0; row--) {
+			for(int column = 0; column < row; column++) {
+				pattern += "* ";
+			}
+			pattern += "\n";
+		}
+		return pattern;
+	}
+	
+	private static String stringTriangle(int size, char character) {
+		String pattern = "";
+		if(size <= 0) {
+			throw new IllegalArgumentException("Size should be greater than 0");
+		}
+		for(int row = size; row > 0; row--) {
+			for(int column = 0; column < row; column++) {
+				pattern += character + " ";
+			}
+			pattern += "\n";
+		}
+		return pattern;
+	}
+	
 	private static void printTriangleUsingWhileLoop(int size) {
 		if(size <= 0) {
 			throw new IllegalArgumentException("Size should be greater than 0");
@@ -38,5 +79,14 @@ public class ForLoopDemo {
 			
 			row--;
 		}
+	}
+	
+	private static void writeFile(String fileName, String data) throws IOException {
+		
+		FileWriter writer = new FileWriter(fileName);
+		BufferedWriter bufferedWriter = new BufferedWriter(writer);
+		
+		writer.write(data);
+		bufferedWriter.close();
 	}
 }
