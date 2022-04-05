f0090d986f5e5a71e6c3dc8c473ce5dcea01c245
Rahul Sahu <therahulsahu7@gmail.com>
2022-04-05 12:43:46 +0530

    post request

--- user-service/src/main/java/com/rakuten/UserController.java
+++ user-service/src/main/java/com/rakuten/UserController.java
@@ -1,6 +1,7 @@
 package com.rakuten;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 	
 	@PostMapping
-	void saveUser() {
-		System.out.println("Called");
+	void saveUser(@RequestBody User user) {
+		System.out.println(user.getName());
+		System.out.println(user.getAge());
 	}
 }
