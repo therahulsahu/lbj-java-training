5fb2720caa36fd406628857ad3f2050e4cb0cdfb
Rahul Sahu <therahulsahu7@gmail.com>
2022-04-05 14:38:36 +0530

    added post mapping demo

--- user-service/src/main/java/com/rakuten/UserController.java
+++ user-service/src/main/java/com/rakuten/UserController.java
@@ -1,5 +1,9 @@
 package com.rakuten;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 	
+	List<User> users = new ArrayList<>();
+	
 	@PostMapping
 	void saveUser(@RequestBody User user) {
 		System.out.println(user.getName());
 		System.out.println(user.getAge());
+		users.add(user);
+	}
+	
+	@GetMapping
+	List<User> getUsers() {
+		System.out.println("Get request done");
+		return users;
 	}
 }
