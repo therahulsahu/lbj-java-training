6c843da06a8e392969e60ce9a8e162ddafe1901c
Rahul Sahu <therahulsahu7@gmail.com>
2022-04-07 15:34:51 +0530

    added type property in Account class

--- account-management/src/main/java/com/rakuten/Account.java
+++ account-management/src/main/java/com/rakuten/Account.java
@@ -10,55 +10,77 @@ enum STATUS {
 	INACTIVE, ACTIVE, CLOSED
 }
 
+enum TYPE {
+	SAVINGS, CURRENT, DEMAT
+}
+
 @Entity
 @Table(name = "accounts")
 public class Account {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
-	
+
 	private String ownerName;
 	private String address;
 	private int balanceAmount;
 	private String createdDate;
 	private STATUS status;
-	
+	private TYPE type;
+
+	public TYPE getType() {
+		return type;
+	}
+
+	public void setType(TYPE type) {
+		this.type = type;
+	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	
+
 	public String getOwnerName() {
 		return ownerName;
 	}
+
 	public void setOwnerName(String ownerName) {
 		this.ownerName = ownerName;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public int getBalanceAmount() {
 		return balanceAmount;
 	}
+
 	public void setBalanceAmount(int balanceAmount) {
 		this.balanceAmount = balanceAmount;
 	}
+
 	public STATUS getStatus() {
 		return status;
 	}
+
 	public void setStatus(STATUS status) {
 		this.status = status;
 	}
-	
+
 	public String getCreatedDate() {
 		return createdDate;
 	}
+
 	public void setCreatedDate(String createdDate) {
 		this.createdDate = createdDate;
 	}
