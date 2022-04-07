43ed49df7cfc90793174dc8af7988ffa419a71d6
Rahul Sahu <therahulsahu7@gmail.com>
2022-04-07 10:31:37 +0530

    moved validations to service class

--- user-service/src/main/java/com/rakuten/UserController.java
+++ user-service/src/main/java/com/rakuten/UserController.java
@@ -1,6 +1,5 @@
 package com.rakuten;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,41 +13,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-	
+
 	@Autowired
 	UserService service;
-	
-	
-	
+
 	@PostMapping
 	void saveUser(@RequestBody User user) {
 		System.out.println(user.getName());
 		System.out.println(user.getAge());
-		
+
 		service.save(user);
 	}
-	
-	
+
 	@GetMapping
 	List<User> getUsers() {
 		System.out.println("Get request done");
 		return service.getAllUsers();
 	}
-	
-	
+
 	@GetMapping("/{name}")
 	List<User> getUsersByName(@PathVariable String name) {
-		if(name.isBlank()) {
-			throw new IllegalArgumentException("Name cannot be blank");
-		}
 		return service.getUserByName(name);
 	}
-	
+
 	@GetMapping("/age/{age}")
 	List<User> getUsersByAge(@PathVariable int age) {
-		if(age <= 0) {
-			throw new IllegalArgumentException("age cannot be less than or equal to 0, age : " + age);
-		}
 		return service.getUserByAge(age);
 	}
 }
--- user-service/src/main/java/com/rakuten/UserService.java
+++ user-service/src/main/java/com/rakuten/UserService.java
@@ -8,28 +8,33 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-	
+
 	List<User> users = new ArrayList<>();
-	
+
 	void save(User user) {
 		users.add(user);
 		System.out.println("Save method called");
 	}
-	
+
 	List<User> getAllUsers() {
 		return users;
 	}
 
 	public List<User> getUserByName(String name) {
-		List<User> filteredUsers = users.stream().filter((user) -> user.getName().equals(name)).collect(Collectors.toList());
+		if (name.isBlank()) {
+			throw new IllegalArgumentException("Name cannot be blank");
+		}
+		List<User> filteredUsers = users.stream().filter((user) -> user.getName().equals(name))
+				.collect(Collectors.toList());
 		return filteredUsers;
 	}
-	
+
 	public List<User> getUserByAge(int age) {
+		if (age <= 0) {
+			throw new IllegalArgumentException("age cannot be less than or equal to 0, age : " + age);
+		}
 		List<User> filteredUsers = users.stream().filter((user) -> user.getAge() == age).collect(Collectors.toList());
 		return filteredUsers;
 	}
-	
-	
-	
+
 }
